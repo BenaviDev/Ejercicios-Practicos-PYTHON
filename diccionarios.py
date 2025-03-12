b1a1e2506a932c8f3fb53bf062aacdf952c04b0a
Santiago Benavidez <sbenavidezr29@gmail.com>
2025-03-12 16:52:02 -0500

    Update diccionarios.py

--- diccionarios.py
+++ diccionarios.py
@@ -1,33 +1,30 @@
+# Definición de un diccionario simple con llaves y valores numéricos
 mi_diccionario = {
-        'llave1': 1,
-        'llave2': 2,
-        'llave3': 3,
-    }
+    'llave1': 1,
+    'llave2': 2,
+    'llave3': 3,
+}
 
+# Diccionario que almacena la población de algunos países
 poblacion_paises = {
-        'Argentina': 45345123,
-        'Colombia': 490534231,
-        'Mexico': 35599443,
-        'Rusia': 34203001,
-    }
+    'Argentina': 45345123,
+    'Colombia': 490534231,
+    'Mexico': 35599443,
+    'Rusia': 34203001,
+}
 
-
-#solo la llave
+# Iterar sobre las claves del diccionario (solo imprime los nombres de los países)
 # for pais in poblacion_paises.keys():
 #     print(pais)
 
-#solo los valores de la llave
-# for pais in poblacion_paises.values():
-#     print(pais)
+# Iterar sobre los valores del diccionario (solo imprime las poblaciones)
+# for poblacion in poblacion_paises.values():
+#     print(poblacion)
 
-#devuelve todos los items
-
-
-#concatenar todo
+# Iterar sobre los elementos del diccionario usando .items()
+# Esto devuelve tuplas con clave y valor (país y su población)
 for pais, poblacion in poblacion_paises.items():
-    print(pais + ' Tiene ' + str(poblacion)+ ' habitantes')
+    print(pais + ' tiene ' + str(poblacion) + ' habitantes')
 
-
-
-print('la poblacion es de: ', poblacion_paises['Mexico'], ' De personas')
-
+# Acceder a un valor específico del diccionario mediante su clave
+print('La población de México es de:', poblacion_paises['Mexico'], 'personas')
